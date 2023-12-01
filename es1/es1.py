bed7dbe60eb82fc63f1aa56fe4601b88841983f5
AndreaDac <andreadacu1996@gmail.com>
2023-12-01 23:32:31 +0100

    first commit

---
+++ es1/es1.py
@@ -0,0 +1,27 @@
+import re
+
+def main1():
+    sum = 0
+    with open("input_simple.txt") as file:
+        for line in file:
+            singleLine = re.findall("[1-9]", line)
+            number = int(singleLine[0] + singleLine[-1])
+            sum = sum + number
+    print(sum)       
+            
+def main2():
+    sum = 0
+    with open("input2.txt") as file:
+        for line in file:
+            singleLine = re.findall ("(?=([1-9]|one|two|three|four|five|six|seven|eight|nine))", line)
+            number = int(numberValue(singleLine[0]) + numberValue(singleLine[-1]))
+            sum = sum + number
+    print(sum)        
+
+def numberValue(numb):
+    dic = {"one":"1", "two":"2", "three":"3", "four":"4", "five":"5", "six":"6", "seven":"7", "eight":"8", "nine":"9"}
+    return dic[numb] if (numb in dic.keys()) else numb
+
+if  __name__ == "__main__":
+    main1()
+    main2()
